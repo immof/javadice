1f987f769416e3e78168dbdedcf173eb28a2accf
Yujin Bok <gddbwls@naver.com>
2022-04-12 19:51:54 +0900

    memberList의 삭제버튼 추가

--- src/main/java/com/herejava/member/dao/MemberDao.java
+++ src/main/java/com/herejava/member/dao/MemberDao.java
@@ -202,6 +202,24 @@ public class MemberDao {
 	}
 
 
+	public int delMember(Connection conn, int memberNo) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String query = "delete from member where member_no=?";
+		try {
+			pstmt=conn.prepareStatement(query);
+			pstmt.setInt(1, memberNo);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(pstmt);
+		}
+		return result;
+	}
+
+
 	
 	
 	
