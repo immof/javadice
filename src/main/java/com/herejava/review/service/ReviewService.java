33b844e43576f9c83703043adbec68bc628304fb
Taeseon Kim <abcc@kakao.com>
2022-04-14 01:28:26 +0900

    회원페이지/리뷰전체불러오기 완성/리뷰 vo생성/마이페이지 이미지불러오기 수정

--- src/main/java/com/herejava/review/service/ReviewService.java
+++ src/main/java/com/herejava/review/service/ReviewService.java
@@ -1,5 +1,22 @@
 package com.herejava.review.service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.herejava.review.dao.ReviewDao;
+import com.herejava.review.vo.Review;
+import com.herejava.review.vo.ReviewList;
+
+import common.JDBCTemplate;
+
 public class ReviewService {
+
+	public ArrayList<ReviewList> myReview(int memberNo) {
+		Connection conn = JDBCTemplate.getConnection();
+		ReviewDao dao = new ReviewDao();
+		ArrayList<ReviewList> reviewList = dao.myReview(conn, memberNo);
+		JDBCTemplate.close(conn);
+		return reviewList;
+	}
 
 }
