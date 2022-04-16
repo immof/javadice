88f67f5bd2dc082bda758c4f0f28bf142989ce10
shin218 <maxjuny@naver.com>
2022-04-16 23:21:43 +0900

    예약취소관련 아직 수정중

--- src/main/java/com/herejava/point/service/PointService.java
+++ src/main/java/com/herejava/point/service/PointService.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 import com.herejava.book.vo.BookPay;
 import com.herejava.book.vo.BookPayData;
+import com.herejava.pay.dao.PayDao;
+import com.herejava.pay.vo.Pay;
 import com.herejava.point.dao.PointDao;
 import com.herejava.point.vo.Point;
 
@@ -32,5 +34,5 @@ public class PointService {
 		JDBCTemplate.close(conn);
 		return result;
 	}
-
+	
 }
